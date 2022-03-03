ca3f7f7342dfb10ca4e56b195a5fb68f1c756d3f
gudetanuki <kuukai.budda@gmail.com>
2022-03-03 23:38:50 +0900

    README記入など

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -2,17 +2,9 @@
 {
   const icon = document.querySelector('.hamburger-icon');
   const overlay = document.querySelector('.overlay');
-  const line1 = document.querySelector('.line-first');
-  const line2 = document.querySelector('.line-second');
-  const line3 = document.querySelector('.line-third');
-  const lines = [line1, line2, line3];
-  console.log(lines);
 
   icon.addEventListener('click', () => {
     overlay.classList.toggle('show');
-    line1.classList.toggle('open');
-    line2.classList.toggle('open');
-    line3.classList.toggle('open');
-    
+    icon.classList.toggle('open');
   });
 }
